ba1ec2a0a70666c48ed976d00b1d6168f4ad629a
Dave Thomson <djthomson@gmail.com>
2016-11-22 15:20:16 +0000

    Getting started tweaks

--- guides/getting-started/index.js
+++ guides/getting-started/index.js
@@ -1,5 +1,5 @@
 const readline = require('readline');
-const chalk = require("chalk");
+ chalk = require("chalk");
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -9,11 +9,11 @@ const rl = readline.createInterface({
 console.log('\nYou take the ' + chalk.blue('blue pill') + ', the story ends.  You wake up in your bed and believe whatever you want to believe.\n');
 console.log('You take the ' + chalk.red('red pill') + ', you stay in Wonderland, and I show you how deep the rabbit hole goes...\n');
 
-rl.question('Would you like the ' + chalk.blue('blue pill') + ' or the ' + chalk.red('red pill') + '?\n', (answer) => {
+rl.question('Would you like the ' + chalk.blue('blue pill') + ' or the ' + chalk.red('red pill') + '?\n\nEnter the colour: ', (answer) => {
   switch (answer) {
       case 'red':
-          console.log(chalk.yellow('\nWell done! you made a wise decision!\n'));
-          console.log(chalk.yellow('Loading matrix...'));
+          console.log(chalk.yellow('\nWell done! you have made a wise decision!\n'));
+          console.log(chalk.yellow('Loading the matrix...'));
           delayedRequire("matrix-rain");
           break;
       case 'blue':
