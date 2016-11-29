28bfae325cc0995f84a24e9008f740e496402afd
Dave Thomson <djthomson@gmail.com>
2016-11-29 07:30:26 +0000

    Rename guide manager

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 var Menu = require('terminal-menu');
 var menu = Menu({ width: 29, x: 4, y: 2 });
-var guides = require('./menu.json')
+var items = require('./menu.json')
 var path = require("path");
 var fs = require('fs');
 var marked = require('marked');
 var TerminalRenderer = require('marked-terminal');
-var GuideManager = require('./lib/guideManager')
-var guide = require('./guides/example/index');
+var Guide = require('./lib/guide')
 var GUIDE_DIR = path.join(__dirname, 'guides')
 
-const gm = new GuideManager(guides, GUIDE_DIR, menu)
+const guide = new Guide(GUIDE_DIR, items, menu)
 
-gm.runMenu()
+guide.runMenu()
